59ddebbeb2686c9d419731b121a468aca97ed39f
hekeki <admin@hekeki.com>
2023-03-19 12:35:56 +0100

    update landing page

--- docs/js/purplesquad.main.js
+++ docs/js/purplesquad.main.js
@@ -4,10 +4,19 @@
         $(window).on('scroll', function (event) {
             let scroll = $(window).scrollTop();
             if (scroll < 20) {
-                $(".navbar-area").removeClass("sticky");
+                $(".navbar").removeClass("navbar-scrolled");
             } else {
-                $(".navbar-area").addClass("sticky");
+                $(".navbar").addClass("navbar-scrolled");
             }
         });
     });
 }(jQuery));
+
+$(document).ready(function(){
+    $('.customers').slick({
+        autoplay: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+});
